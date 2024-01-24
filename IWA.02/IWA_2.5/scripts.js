1fe094ceed9f8cfd7227c4f9eea5385c783bb04e
JaredCarelse <148363511+JaredCarelse@users.noreply.github.com>
2024-01-24 18:39:13 +0200

    Add files via upload

---
+++ IWA.02/IWA_2.5/scripts.js
@@ -0,0 +1,10 @@
+/** (c) ACME Inc. 2010 */
+
+/*
+ * This is the date that a user created their account 
+ */
+const dateString = '10/07/2014'
+
+const date = new Date(dateString);
+
+console.log(date)
